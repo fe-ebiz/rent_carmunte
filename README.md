6af01aa5c01932cd9f5c8903f0554683bd5c2cd0
ahehfldi007 <ahehfldi007@naver.com>
2021-03-24 17:30:33 +0900

    first

---
+++ README.md
@@ -0,0 +1,21 @@
+## 렌트카
+
+## http://carminute.co.kr/
+
+## 카민 테스트
+## http://design.ebiznetworks.co.kr/lhc/carminute/main/index.html
+## http://design.ebiznetworks.co.kr/lhc/carminute/shoping/index.html
+
+## 카민 관리자
+## http://design.ebiznetworks.co.kr/lhc/carminute/master/master_test.html
+## http://design.ebiznetworks.co.kr/lhc/carminute/master/customer_test.html
+
+
+## 카민
+## http://design.ebiznetworks.co.kr/lhc/rent_car/landing/car_minute/
+
+## 올유어카
+## http://design.ebiznetworks.co.kr/lhc/rent_car/landing/allyourcar/
+
+## 멜론카
+## http://design.ebiznetworks.co.kr/lhc/rent_car/landing/meloncar/
---
+++ src/assets/js/common/common.js
@@ -0,0 +1,97 @@
+//input 자동완성 기능끄기
+$('input').attr('autocomplete','off');
+
+
+$(document).ready(function(){
+    //메뉴 관련
+    let anchorOn = false;
+    let anchorList = $(".header .main-menu-ul>li>a");
+    anchorList.on("click", function(e){
+        
+        let anchorName = $(this).data('move-anchor');
+        if(anchorName == undefined){
+            return false;
+        }
+        let correctPos = 83;
+        if(window.innerWidth <= 900){
+            correctPos = 0;
+        }
+
+        anchorOn = true;
+        let anchorBox = document.getElementsByClassName(anchorName);
+        let anchorTop = $(anchorBox).offset().top - correctPos;
+        $('html').animate({
+            scrollTop: anchorTop
+        },600,
+        function(){
+            // setMenuActiveCheck();
+            anchorOn = false;
+        });
+    });
+
+
+});
+
+
+/*스크롤 업*/
+$(document).ready(function(){
+    $(".fn-up-wrap").hide();
+    $(".fn-up-wrap").css("oparcity","0");
+    $(window).on("scroll", function(){
+        console.log((window.scrollY > 600));
+        if(window.scrollY > 700){
+            $(".fn-up-wrap").show();
+            $(".fn-up-wrap").stop().animate({ "opacity": '1'},200);
+        }
+        else{
+            $(".fn-up-wrap").stop().animate({ "opacity": '0'}, 200,
+            function(){
+                $(".fn-up-wrap").hide();
+            });
+        }
+    });
+
+    $(".fn-scroll-up").on("click", function(){
+        $('html, body').animate( { scrollTop : '0' }, 300);
+    });
+});
+
+/*모바일일 때 적용(팝업버튼이 견적이동 버튼으로)*/
+$(document).ready(function(){
+    let winW = window.outerWidth;
+    if(winW <= 900){
+        let = shopBtn = $(".sect-shoping .car-info-box .btn-quote");
+        shopBtn.removeClass("btn-open-consult");
+        shopBtn.addClass("btn-move-quote");
+
+        let = headerBtn = $(".header .main-menu-ul .btn-open-consult");
+        headerBtn.removeClass("btn-open-consult");
+        headerBtn.addClass("btn-move-quote");
+    }
+});
+
+
+/*가기버튼*/
+$(document).ready(function(){
+    $(".btn-bottom-sect .btn-estimate").removeAttr("href");
+    $(".btn-move-quote").on("click", function(){
+        let quoteBox = $(".sect-quote")
+        if(quoteBox == undefined){
+            return false;
+        }
+        let correctPos = 83;
+        if(window.innerWidth <= 900){
+            correctPos = 0;
+        }
+        
+        let anchorTop = quoteBox.offset().top - correctPos;
+        $('html').animate({
+            scrollTop: anchorTop
+        },600,
+        function(){
+        
+        });
+    });
+});
+
+
